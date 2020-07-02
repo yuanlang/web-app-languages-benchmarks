507c7fe962b0a4e9a9555e79dc443e144e3b549b
2448338y <2448338y@student.gla.ac.uk>
2020-07-02 20:45:31 +0100

    add file

---
+++ ProcessCreationLatency/rust/testLimit.rs
@@ -0,0 +1,17 @@
+fn go() {
+	std::thread::sleep(std::time::Duration::from_millis(10000));
+}
+
+fn main() {
+    let mut cnt = 0;
+    loop {
+        match std::thread::Builder::new().spawn(go) {
+            Ok(_) => cnt += 1,
+            Err(e) => {
+                println!("error: {:?} {:?}", e.kind(), e);
+                println!("cnt {}", cnt);
+                return
+            }
+        }
+    }
+}
