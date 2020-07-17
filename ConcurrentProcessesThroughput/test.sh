324c593cb40aad3d5ab148876e59d3fe07f9e8aa
2448338y <2448338y@student.gla.ac.uk>
2020-07-17 10:51:09 +0100

    add test script for ConcurrentProcessesThroughput

---
+++ ConcurrentProcessesThroughput/test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+filename="test.result.`date +"%Y%m%d-%H%M%S"`"
+for lang in go erlang scala
+do
+cd $lang
+echo "process language: $lang"
+echo "process language: $lang" >> $filename
+for k in 1 2 3
+do
+    echo "Test ConcurrentProcessesThroughput time $k"
+    echo "Test ConcurrentProcessesThroughput time $k" >> $filename
+    for c in 0x01 0x03 0x0f 0xff 0xffff 0xffffffff
+    do
+        echo "Test for core number $c"
+	echo "Test for core number $c" >> $filename
+        for i in 1 2 4 6 8 16 32 64 128
+        do
+            echo "Looping ... number $i"
+	    echo "Looping ... number $i" >> $filename
+       	    if [ $lang = "go" ]; then
+                taskset $c ./maxSpeakers -n $i >> $filename
+            elif [ $lang = "erlang" ]; then
+                taskset $c erl -noshell -run maxSpeakers benchmark $i >> $filename &
+                sleep 65
+                killall beam.smp
+            elif [ $lang = "scala" ]; then
+                taskset $c sbt "run $i" >> $filename
+            fi
+        done
+    done	
+done
+cd ..
+done
+
