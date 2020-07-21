3a8e3face0be4a0da780ff24148b5238cb4c563f
2448338y <2448338y@student.gla.ac.uk>
2020-07-21 20:29:18 +0100

    change to 2 sender and 2 receiver threads

--- ProcessCommunicationLatency/rust/src/main.rs
+++ ProcessCommunicationLatency/rust/src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 use std::sync::mpsc;
 use std::thread;
-//use std::io::Write;
+use std::sync::Arc;
 use std::time::{Instant};
+use std::time::Duration;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,46 +19,68 @@ fn main() {
     let d: u32 = arg2.parse().expect("Not a number!");
     println!("Repeat times: {}, data length: {}", r, d);
 
-    let send_bytes_1: Vec<u8> = (0..d).map(|_| { rand::random::<u8>() }).collect();
+    let send_bytes_1 : Arc<Vec<u8>> = Arc::new((0..d).map(|_| { rand::random::<u8>() }).collect());
     //println!("{:?}", send_bytes_1);
-    let send_bytes_2: Vec<u8> = (0..d).map(|_| { rand::random::<u8>() }).collect();
+    let send_bytes_2 : Arc<Vec<u8>> = Arc::new((0..d).map(|_| { rand::random::<u8>() }).collect());
     //println!("{:?}", send_bytes_2);
 
-    let (tx1, _rx1) = mpsc::channel::<Vec<u8>>();
-    let (tx2, _rx2) = mpsc::channel::<Vec<u8>>();
+    let (tx1, _rx1) = mpsc::channel::<Arc<Vec<u8>>>();
+    let (tx2, _rx2) = mpsc::channel::<Arc<Vec<u8>>>();
 
     let start = Instant::now();
     let p1 = thread::spawn(move || {
         // let val = String::from("hi");
         let mut num = 0;
         while num < r {
-            let sending = send_bytes_1.to_vec();
+            let sending = send_bytes_1.clone();
             tx1.send(sending).unwrap();
             num += 1;
         }
         
-        //let received = _rx2.recv().unwrap();
-        //println!("Got: {:?}", received.len());
+        // let received = _rx2.recv().unwrap();
+        // println!("Got: {:?}", received.len());
     });
 
     let p2 = thread::spawn(move || {
         // let val = String::from("hi");
         let mut num = 0;
         while num < r {
-            let sending = send_bytes_2.to_vec();
+            let sending = send_bytes_2.clone();
             tx2.send(sending).unwrap();
             num += 1;
         }
-        //let received = _rx1.recv().unwrap();
-        //println!("Got: {:?}", received.len());
+        // let received = _rx1.recv().unwrap();
+        // println!("Got: {:?}", received.len());
+    });
+
+    let p3 = thread::spawn(move || {
+        let d = Duration::from_millis(10);
+        let mut num = 0;
+        while num < r {
+            //println!("p3 recv");
+            let _r = _rx1.recv_timeout(d);
+            num += 1;
+        }
     });
     
+    let p4 = thread::spawn(move || {
+        let d = Duration::from_millis(10);
+        let mut num = 0;
+        while num < r {
+            //println!("p4 recv");
+            let _r = _rx2.recv_timeout(d);
+            num += 1;
+        }
+    });
+
     p1.join().unwrap();  
     p2.join().unwrap();
+    p3.join().unwrap();  
+    p4.join().unwrap();
 
     let duration = start.elapsed();
 
-    println!("Total time taken: {:?}s", duration.as_secs_f64());
+    println!("Total time taken: {:?} seconds", duration.as_secs_f64());
 
     std::process::exit(0);
 }
