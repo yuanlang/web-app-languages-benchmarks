5af2b85b632f4cdecdeefc418a65390f4a797341
Lang Yuan <peacocker@163.com>
2020-08-21 08:23:13 +0100

    add timestamp

--- ProcessCreationLatency/rust/test_limit_tokio/src/main.rs
+++ ProcessCreationLatency/rust/test_limit_tokio/src/main.rs
@@ -1,23 +1,28 @@
 // use tokio::net::TcpListener;
-use tokio::prelude::*;
+// use tokio::prelude::*;
 use tokio::time;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-async fn task_that_takes_a_second(num : i32) {
-    println!("hello {}", num);
-    time::delay_for(time::Duration::from_secs(5)).await
+async fn task_that_takes_a_second() {
+    // println!("hello {}", num);
+    time::delay_for(time::Duration::from_secs(10000)).await
 }
 
 #[tokio::main]
 async fn main() {
     // let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
-    let mut i = 0;
-    while i < 100 {
+    let mut i = 1;
+    loop {
         // let (mut socket, _) = listener.accept().await?;
-        let num = i;
         tokio::spawn(async move {
-            task_that_takes_a_second(num).await;
+            task_that_takes_a_second().await;
         });
+        
+        match SystemTime::now().duration_since(UNIX_EPOCH) {
+            Ok(n) => println!("ts(sec): {:?} thd no: {}", n.as_secs(), i),
+            Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+        }
         i += 1;
     }
-    return;
+    // return;
 }
