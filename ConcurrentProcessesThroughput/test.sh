e080ee2416478530f860f1f56a28e0a14d5fe529
Lang Yuan <peacocker@163.com>
2020-08-31 12:47:35 +0800

    modify the ouput

--- ConcurrentProcessesThroughput/test.sh
+++ ConcurrentProcessesThroughput/test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 filename="test.result.`date +"%Y%m%d-%H%M%S"`"
-#for lang in go erlang scala
-for lang in rust
+for lang in go erlang scala
 do
 cd $lang
 echo "process language: $lang"
@@ -13,11 +12,11 @@ do
     for c in 0x01 0x03 0x0f 0xff 0xffff 0xffffffff
     do
         echo "Test for core number $c"
-	echo "Test for core number $c" >> $filename
+        echo "Test for core number $c" >> $filename
         for i in 1 2 4 6 8 16 32 64 128
         do
-            echo "Looping ... number $i"
-	    echo "Looping ... number $i" >> $filename
+            echo "Looping thread number $i"
+            echo "Looping thread number $i" >> $filename
        	    if [ $lang = "go" ]; then
                 taskset $c ./maxSpeakers -n $i >> $filename
             elif [ $lang = "erlang" ]; then
