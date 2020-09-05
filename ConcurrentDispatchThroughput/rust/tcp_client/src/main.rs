40a1479ecf26db779469c933570279703b33ee71
Lang Yuan <peacocker@163.com>
2020-09-05 10:32:31 +0800

    parallel the generator

--- ConcurrentDispatchThroughput/rust/tcp_client/src/main.rs
+++ ConcurrentDispatchThroughput/rust/tcp_client/src/main.rs
@@ -2,8 +2,11 @@ use std::env;
 use std::net::{Shutdown, TcpStream};
 use std::io::{Write};
 use rand::{thread_rng, Rng};
+use std::error::Error;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("invalid number of connections");
@@ -21,23 +24,11 @@ fn main() {
 
     for curr in 0 .. connect_num {
         match TcpStream::connect("localhost:8888") {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("No.{} Successfully connected to server in port 8888", curr);
-                
-                for _i in 0 .. repeat_num / connect_num {
-                    let msg_len = 500; //data length
-                    let mut send_bytes : Vec<u8> = (0..msg_len).map(|_| { rand::random::<u8>() }).collect();
-
-                    // gen the msg type
-                    let mut rng = thread_rng();
-                    let n: u8 = rng.gen_range(1, 11);
-
-                    send_bytes[0] = n;
-                    stream.write(&send_bytes).unwrap();
-                    println!("Sent msg to No.{} receiver ", n);
-                }
-
-                stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+                tokio::spawn(async move {
+                    do_send_and_close(repeat_num / connect_num, stream).await;
+                });
             },
             Err(e) => {
                 println!("Failed to connect: {}", e);
@@ -45,4 +36,22 @@ fn main() {
         }
     }
     println!("Terminated.");
+    Ok(())
+}
+
+async fn do_send_and_close(repeat_num: u32, mut stream: TcpStream) {
+    for _i in 0 .. repeat_num {
+        let msg_len = 500; //data length
+        let mut send_bytes : Vec<u8> = (0..msg_len).map(|_| { rand::random::<u8>() }).collect();
+
+        // gen the msg type
+        let mut rng = thread_rng();
+        let n: u8 = rng.gen_range(1, 11);
+
+        send_bytes[0] = n;
+        stream.write(&send_bytes).unwrap();
+        println!("Sent msg to No.{} receiver ", n);
+    }
+
+    stream.shutdown(Shutdown::Both).expect("shutdown call failed");
 }
