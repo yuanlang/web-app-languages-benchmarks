555bc57acc46da0ed9bf4231eafc75c7e8cf664a
Lang Yuan <peacocker@163.com>
2020-09-06 20:24:25 +0800

    change back to bounded becuase in unbounded mode, the dispatcher cannot receive any messages

--- ConcurrentDispatchThroughput/rust/dispatcher/src/main.rs
+++ ConcurrentDispatchThroughput/rust/dispatcher/src/main.rs
@@ -10,6 +10,7 @@
 // use tokio::io::{AsyncReadExt, AsyncWriteExt};
 // use tokio::net::TcpListener;
 use tokio::sync::mpsc;
+use tokio::runtime::Runtime;
 // use tokio::net::{TcpStream};
 use std::collections::VecDeque;
 
@@ -22,7 +23,8 @@ use std::thread;
 use std::time::{Instant};
 use std::time::Duration;
 
-const MSG_LEN: usize = 500; //data length
+const MSG_LEN: usize = 1500; //data length
+const MSG_QUEUE_LEN: usize = 100; //message queue length
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -45,18 +47,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let total_msg_num: u32 = arg3.parse().expect("Not a number!");
     println!("Total message number: {}", total_msg_num);
 
+    let runtime = Runtime::new().unwrap();
+
     // Create a channel for generators to dispatcher, 
     // one sender could share with multi generator
     let (chan_tx_to_disp, chan_rx_by_disp) = 
-                    mpsc::unbounded_channel::<[u8; MSG_LEN]>();
+                    mpsc::channel::<[u8; MSG_LEN]>(MSG_QUEUE_LEN * MSG_LEN);
 
     // Create a group of channels for dispatcher to receivers
     // dispatcher hold all the sender, and dispatch message by the first byte of a message
     // hashmap: key = receiver_id from 1 : number of receiver, 0 reserve for futher use
-    let mut channels_tx_map : HashMap<u32, mpsc::UnboundedSender<[u8; MSG_LEN]>>  = HashMap::new();
-    let mut channels_rx_vec: VecDeque<mpsc::UnboundedReceiver<[u8; MSG_LEN]>> = VecDeque::new();
+    let mut channels_tx_map : HashMap<u32, mpsc::Sender<[u8; MSG_LEN]>>  = HashMap::new();
+    let mut channels_rx_vec: VecDeque<mpsc::Receiver<[u8; MSG_LEN]>> = VecDeque::new();
     for curr in 1 .. receiver_num + 1 {
-        let (sender, receiver) = mpsc::unbounded_channel::<[u8; MSG_LEN]>();
+        let (sender, receiver) = mpsc::channel::<[u8; MSG_LEN]>(MSG_QUEUE_LEN * MSG_LEN);
         channels_tx_map.insert(curr, sender);
         channels_rx_vec.push_back(receiver);
     }
@@ -70,7 +74,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("Successfully create receiver {}", curr);
 
         // get the channel receiver first
-        tokio::spawn(async move {
+        runtime.spawn(async move {
             do_receive(curr, rx1, recv_mutex).await;
         });
 
@@ -81,7 +85,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("Successfully create dispatcher");
     let disp_counter: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
     let disp_mutex = Arc::clone(&disp_counter);
-    tokio::spawn(async move {
+    runtime.spawn(async move {
         do_dispatch(chan_rx_by_disp, channels_tx_map, disp_mutex).await;
     });
 
@@ -97,9 +101,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         // all clients to make progress concurrently, rather than
         // blocking one on completion of another. To achieve this we use the
         // `tokio::spawn` function to execute the work in the background.
-        tokio::spawn(async move {
-            do_generate(chan_tx_to_disp_copy, receiver_num as u8, 
-                total_msg_num / generator_num, gene_mutex).await;
+        runtime.spawn(async move {
+            do_generate(chan_tx_to_disp_copy, receiver_num as u8, gene_mutex).await;
         });
     }
 
@@ -114,10 +117,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let duration = start.elapsed();
 
-    println!("Total time taken seconds: {:?} ", duration.as_secs_f64());
-    println!("Total generated messages: {} ", gene_num);
-    println!("Total dispatched messages: {} ", disp_num);
-    println!("Total received messages: {} ", recv_num);
+    println!("Total time taken seconds:\t {:?} ", duration.as_secs_f64());
+    println!("Total generated messages:\t {} ", gene_num);
+    println!("Total dispatched messages:\t {} ", disp_num);
+    println!("Total received messages:\t {} ", recv_num);
+
+    runtime.shutdown_background();
 
     Ok(())
 }
@@ -127,7 +132,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 /// Receive messages from channel and dispatch them to the Receiver through 
 /// the TCP connnections
 async fn do_receive(_id: usize, 
-        mut rx: mpsc::UnboundedReceiver<[u8; MSG_LEN]>, 
+        mut rx: mpsc::Receiver<[u8; MSG_LEN]>, 
         counter: Arc<Mutex<usize>>) {
     loop {
         // Read message from the channel and wait replay
@@ -155,8 +160,8 @@ async fn do_receive(_id: usize,
 /// Parameter:
 /// 
 async fn do_dispatch(
-        mut chan_rx: mpsc::UnboundedReceiver<[u8; MSG_LEN]>, 
-        mut chan_tx_map: std::collections::HashMap<u32, mpsc::UnboundedSender<[u8; MSG_LEN]>>,
+        mut chan_rx: mpsc::Receiver<[u8; MSG_LEN]>, 
+        mut chan_tx_map: std::collections::HashMap<u32, mpsc::Sender<[u8; MSG_LEN]>>,
         counter: Arc<Mutex<usize>>) {
     loop {
         // Read message from the channel and wait replay
@@ -168,7 +173,7 @@ async fn do_dispatch(
                 //dispatch msg to a Receiver
                 match chan_tx_map.get_mut(&receiver_id) {
                     Some(tx_copy) => {
-                        if let Err(_) = tx_copy.send(msg) {
+                        if let Err(_) = tx_copy.send(msg).await {
                             println!("receiver thread dropped");
                             return;
                         }
@@ -195,21 +200,21 @@ async fn do_dispatch(
 /// 
 /// Receive message from the Generator and send the message to the corresponed receiver
 /// by using the first byte of the message
-async fn do_generate(chan_tx_to_disp: mpsc::UnboundedSender<[u8; MSG_LEN]>, 
-        receiver_num: u8, msg_num: u32,
+async fn do_generate(mut chan_tx_to_disp: mpsc::Sender<[u8; MSG_LEN]>, 
+        receiver_num: u8,
         counter: Arc<Mutex<usize>>) {
     let send_bytes : Vec<u8> = (0..MSG_LEN).map(|_| { rand::random::<u8>() }).collect();
     let mut send_array = [0u8; MSG_LEN];
     send_array.copy_from_slice(&send_bytes);
 
     // In a loop, read data from the socket and write the data back.
-    for _i in 0 .. msg_num{
+    loop {
         // gen receiver id
         let n = rand::random::<u8>() % receiver_num + 1;
         send_array[0] = n;
 
         // send message to dispatcher
-        if let Err(_) = chan_tx_to_disp.send(send_array) {
+        if let Err(_) = chan_tx_to_disp.send(send_array).await {
             println!("receiver thread dropped");
             return;
         }
