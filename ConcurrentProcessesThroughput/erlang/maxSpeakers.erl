ec0b6cdd2d8413dfd577245af1b47a2d721aaa2e
Lang Yuan <peacocker@163.com>
2020-09-07 10:12:14 +0800

    format

--- ConcurrentProcessesThroughput/erlang/maxSpeakers.erl
+++ ConcurrentProcessesThroughput/erlang/maxSpeakers.erl
@@ -1,19 +1,23 @@
 -module(maxSpeakers).
--compile(export_all).
+-export([benchmark/1]).
 
 benchmark([N]) ->
     process_flag(priority, max),
     Start = erlang:system_time(millisecond),
-    Monitor = spawn(?MODULE, aggregator, [0, erlang:system_time(millisecond)]),
+    Monitor = spawn(?MODULE, aggregator(0, erlang:system_time(millisecond))),
     NumOfServers = element(1, string:to_integer(N)),
     Servers = serverSpawner(Monitor, NumOfServers, bytes_generate(500)),
     End = erlang:system_time(millisecond),
     io:format("Time taken to create servers: ~f seconds~n", [(End-Start)/1000]),
+
+    % phrase start
     Start2 = erlang:system_time(millisecond),
 
     [Pid ! "Start" || Pid <- Servers],
-    %wait for the processes to finish
+    
+    %wait a period time, then calculate the msg count
     timer:sleep(1000*60),
+
     Monitor ! "Show",
     receive
         "Done" ->
@@ -21,16 +25,17 @@ benchmark([N]) ->
             io:format("Time taken to do the work: ~f seconds~n", [(End2-Start2)/1000])
     end.
 
-serverSpawner(Monitor, 0, D) -> [];
+serverSpawner(_Monitor, 0, _D) -> [];
 
 serverSpawner(Monitor, N, D) when N>0 ->
     io:format("N: ~w times~n", [N]),
-    [spawn(?MODULE, server, [Monitor, clientSpawner(1), 0, D]) | serverSpawner(Monitor, N-1, D) ].    
+    [spawn(?MODULE, server(Monitor, clientSpawner(1), 0, D)) 
+        | serverSpawner(Monitor, N-1, D) ].
 
 clientSpawner(0) -> [];
 
 clientSpawner(N) when N>0 ->
-    [spawn(?MODULE, client, []) | clientSpawner(N-1) ].  
+    [spawn(?MODULE, client()) | clientSpawner(N-1)].
 
 server(Monitor, Clients, 100, Data) ->
     Monitor ! 100,
@@ -39,6 +44,8 @@ server(Monitor, Clients, 100, Data) ->
 server(Monitor, Clients, MessagesCount, Data) ->
     receive
         "Start" ->
+            % ? why send message to all clients?
+            % Although use clients, it only has one instance
             [Pid ! {"Hello",self(), Data} || Pid <- Clients],
             server(Monitor, Clients, MessagesCount, Data);
         {"Done", Pid, Data} ->
--- ConcurrentProcessesThroughput/erlang/test.sh
+++ ConcurrentProcessesThroughput/erlang/test.sh
@@ -6,15 +6,14 @@ do
     for c in 0x01 0x03 0x0f 0xff 0xffff 0xffffffff
     do
         echo "Test for core number $c"
-	echo "Test for core number $c" >> test.result
+        echo "Test for core number $c" >> test.result
         for i in 1 2 4 6 8 16 32 64 128
         do
             echo "Looping ... number $i"
-	    echo "Looping ... number $i" >> test.result
+            echo "Looping ... number $i" >> test.result
             taskset $c erl -noshell -run maxSpeakers benchmark $i >> test.result &
-	    sleep 65
-	    killall beam.smp
+            sleep 65
+            killall beam.smp
         done
-    done	
+    done    
 done
-
