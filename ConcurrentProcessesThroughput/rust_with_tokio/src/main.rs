badae3b68b9e67fb1ee9e08baf70d9f71c2e05d1
Lang Yuan <peacocker@163.com>
2020-09-08 16:01:19 +0800

    add aggregator for rust and rust with tokio

--- ConcurrentProcessesThroughput/rust_with_tokio/src/main.rs
+++ ConcurrentProcessesThroughput/rust_with_tokio/src/main.rs
@@ -1,15 +1,19 @@
 use std::env;
-use std::sync::mpsc;
+use std::error::Error;
 use std::thread;
 use std::time::{Instant};
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
 
-const MSG_LENGTH: usize = 500; //data length
-const TEST_DURATION: u64 = 60; //the length of test, in seconds
-const FREQUENT: usize = 1000; //report per time
+use tokio::sync::mpsc;
 
-fn main() {
+const MSG_LENGTH: usize = 500; //data length
+const MSG_QUEUE_LENGTH: usize = 100;
+const TEST_DURATION: u64 = 60; //the length of test, in seconds
+const FREQUENT: usize = 100;  //report messages per time
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         print!("invalid number of goroutines\n");
@@ -24,15 +28,14 @@ fn main() {
     let send_bytes : Vec<u8> = (0..MSG_LENGTH).map(|_| { rand::random::<u8>() }).collect();
     //println!("{:?}", send_bytes_1);
 
-    let tx_counter = Arc::new(Mutex::new(0));
+    let (tx_to_aggregator, mut rx_by_aggregator) = mpsc::channel::<usize>(MSG_QUEUE_LENGTH);
+    
+    let tx_counter: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
 
-    let (tx_to_aggregator, rx_by_aggregator) = mpsc::channel::<usize>();
-
-    //create aggregator to calculate the count
+    // spawn aggregator to recv counter report
     let counter1 = Arc::clone(&tx_counter);
-    thread::spawn(move || {
-        while let Ok(count) = rx_by_aggregator.recv() {
-            // received message means one round finish, an increase of 1
+    tokio::spawn(async move {
+        while let Some(count) = rx_by_aggregator.recv().await {
             let mut num = counter1.lock().unwrap();
             *num += count;
         }
@@ -43,36 +46,35 @@ fn main() {
     let mut server_thread_holder = vec![];
     let mut client_thread_holder = vec![];
     for _i in 0 .. n {
-        let (tx_to_client, rx_by_server) = mpsc::channel::<Vec<u8>>();
-        let (tx_to_server, rx_by_client) = mpsc::channel::<Vec<u8>>();
-        // let tx_to_client = mpsc::Sender::clone(&tx);
+        let (mut tx_to_client, mut rx_by_server) = mpsc::channel::<Vec<u8>>(MSG_LENGTH * MSG_QUEUE_LENGTH);
+        let (mut tx_to_server, mut rx_by_client) = mpsc::channel::<Vec<u8>>(MSG_LENGTH * MSG_QUEUE_LENGTH);
+        let mut tx_to_aggregator_copy = mpsc::Sender::clone(&tx_to_aggregator);
         // let counter1 = Arc::clone(&tx_counter);
         let send_bytes_copy = send_bytes.clone();
-        let tx_to_aggregator_copy = tx_to_aggregator.clone();
         
         //spawn server threads
-        server_thread_holder.push(thread::spawn(move || {
+        server_thread_holder.push(tokio::spawn(async move {
             let sending = send_bytes_copy.clone();
-            tx_to_client.send(sending.to_vec()).unwrap();
-            let mut counter: usize = 0;
-            while let Ok(msg) = rx_by_server.recv() {
+            let mut counter = 0;
+            tx_to_client.send(sending.to_vec()).await.unwrap();
+            while let Some(msg) = rx_by_server.recv().await {
                 // received message means one round finish, an increase of 1
                 // let mut num = counter1.lock().unwrap();
                 // *num += 1;
                 counter += 1;
                 if counter == FREQUENT {
-                    // send message to aggragetor
-                    tx_to_aggregator_copy.send(FREQUENT).unwrap();
+                    //send message to aggregator when the counter is 100 and reset it to 0
+                    tx_to_aggregator_copy.send(FREQUENT).await.unwrap();
                     counter = 0;
                 }
-                tx_to_client.send(msg).unwrap();
+                tx_to_client.send(msg).await.unwrap();
             }
         }));
 
         //spawn client threads
-        client_thread_holder.push(thread::spawn(move || {
-            while let Ok(msg) = rx_by_client.recv() {
-                tx_to_server.send(msg).unwrap();
+        client_thread_holder.push(tokio::spawn(async move {
+            while let Some(msg) = rx_by_client.recv().await {
+                tx_to_server.send(msg).await.unwrap();
             }
         }));
     }
