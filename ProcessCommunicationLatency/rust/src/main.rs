48a57173dc80953734280a7e0787c55a9fbfb3a3
Lang Yuan <peacocker@163.com>
2020-09-08 11:43:10 +0800

    modify the send and recv method

--- ProcessCommunicationLatency/rust/src/main.rs
+++ ProcessCommunicationLatency/rust/src/main.rs
@@ -3,7 +3,7 @@ use std::sync::mpsc;
 use std::thread;
 //use std::sync::Arc;
 use std::time::{Instant};
-use std::time::Duration;
+// use std::time::Duration;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,63 +20,57 @@ fn main() {
     println!("Repeat times: {}, data length: {}", r, d);
 
     let send_bytes_1 : Vec<u8> = (0..d).map(|_| { rand::random::<u8>() }).collect();
-    //println!("{:?}", send_bytes_1);
     let send_bytes_2 : Vec<u8> = (0..d).map(|_| { rand::random::<u8>() }).collect();
-    //println!("{:?}", send_bytes_2);
 
-    let (tx1, _rx1) = mpsc::channel::<Vec<u8>>();
-    let (tx2, _rx2) = mpsc::channel::<Vec<u8>>();
+    let (tx1, rx1) = mpsc::channel::<Vec<u8>>();
+    let (tx2, rx2) = mpsc::channel::<Vec<u8>>();
 
     let start = Instant::now();
     let p1 = thread::spawn(move || {
-        // let val = String::from("hi");
         let mut num = 0;
         while num < r {
             let sending = send_bytes_1.clone();
             tx1.send(sending.to_vec()).unwrap();
             num += 1;
+            rx2.recv().unwrap();
         }
         
-        // let received = _rx2.recv().unwrap();
-        // println!("Got: {:?}", received.len());
     });
 
     let p2 = thread::spawn(move || {
-        // let val = String::from("hi");
         let mut num = 0;
         while num < r {
             let sending = send_bytes_2.clone();
             tx2.send(sending.to_vec()).unwrap();
             num += 1;
+            rx1.recv().unwrap();
         }
-        // let received = _rx1.recv().unwrap();
-        // println!("Got: {:?}", received.len());
     });
 
-    let p3 = thread::spawn(move || {
-        let d = Duration::from_millis(10);
-        let mut num = 0;
-        while num < r {
-            //println!("p3 recv");
-            let _r = _rx1.recv_timeout(d);
-            num += 1;
-        }
-    });
+    // let p3 = thread::spawn(move || {
+    //     let d = Duration::from_millis(10);
+    //     let mut num = 0;
+    //     while num < r {
+    //         //println!("p3 recv");
+    //         let _r = rx1.recv_timeout(d);
+    //         num += 1;
+    //     }
+    // });
     
-    let p4 = thread::spawn(move || {
-        let d = Duration::from_millis(10);
-        let mut num = 0;
-        while num < r {
-            //println!("p4 recv");
-            let _r = _rx2.recv_timeout(d);
-            num += 1;
-        }
-    });
+    // let p4 = thread::spawn(move || {
+    //     let d = Duration::from_millis(10);
+    //     let mut num = 0;
+    //     while num < r {
+    //         //println!("p4 recv");
+    //         let _r = rx2.recv_timeout(d);
+    //         num += 1;
+    //     }
+    // });
 
     p1.join().unwrap();  
     p2.join().unwrap();
-    p3.join().unwrap();  
-    p4.join().unwrap();
+    // p3.join().unwrap();  
+    // p4.join().unwrap();
 
     let duration = start.elapsed();
 
@@ -85,12 +79,3 @@ fn main() {
     std::process::exit(0);
 }
 
-/*
-/// Copy data in `from` into `to`, until the shortest
-/// of the two slices.
-///
-/// Return the number of bytes written.
-fn byte_copy(from: &[u8], mut to: &mut [u8]) -> usize {
-    to.write(from).unwrap()
-}
-*/
