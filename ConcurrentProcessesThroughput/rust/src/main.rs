6f890ba8e3780b31a1130c5053bb11615aa20d49
Lang Yuan <peacocker@163.com>
2020-09-08 14:54:32 +0800

    msg length as const

--- ConcurrentProcessesThroughput/rust/src/main.rs
+++ ConcurrentProcessesThroughput/rust/src/main.rs
@@ -5,6 +5,8 @@ use std::time::{Instant};
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
 
+const MSG_LENGTH: usize = 500; //data length
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -17,8 +19,7 @@ fn main() {
     let n: u32 = arg1.parse().expect("Not a number!");
     println!("Thread number: {}", n);
 
-    let d = 500; //data length
-    let send_bytes : Vec<u8> = (0..d).map(|_| { rand::random::<u8>() }).collect();
+    let send_bytes : Vec<u8> = (0..MSG_LENGTH).map(|_| { rand::random::<u8>() }).collect();
     //println!("{:?}", send_bytes_1);
 
     // let (tx_to_aggregator, rx_by_aggregator) = mpsc::channel::<u32>();
